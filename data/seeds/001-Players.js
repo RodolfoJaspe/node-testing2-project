58d63fab1d950f6c30422ef19580a4c07ca0f335
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-10-08 00:15:18 -0400

    created database and seeded data

--- data/seeds/001-Players.js
+++ data/seeds/001-Players.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+const players = [
+    { player_name: 'Paul Pogba',goals:0,assists:7,appearances:9},
+    { player_name: 'Cristiano Ronaldo',goals:5,assists:0,appearances:6},
+    { player_name: 'Lionel Messi',goals:1,assists:0,appearances:5 },
+    { player_name: 'Kylian Mbappé',goals:4,assists:3,appearances:11 },
+  ]
+  
+  exports.players = players
+  
+  exports.seed = function (knex) {
+    return knex('players').insert(players)
+  }
